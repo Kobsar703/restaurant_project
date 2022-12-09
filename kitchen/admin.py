1214abb897c821d629252db3646eb8c7df1650b0
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-12-09 19:48:34 -0400

    Added urls file to kitchen app and redirected from urls in kitchen_service

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from kitchen.models import Ingredient, DishType, Dish, Cook
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + ((("Additional info",), {"fields": ("years_of_experience",)}),)
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "dish_type"]
+    list_filter = ["price"]
+    search_fields = ["name"]
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ["name"]
