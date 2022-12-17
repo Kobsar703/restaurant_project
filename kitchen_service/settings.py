6395399f094404f320296cffc565ee0124338955
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-12-16 21:39:08 -0400

    Staticfiles manage

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -86,6 +89,10 @@ LOGIN_REDIRECT_URL = ""
 DATABASES = {
     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
 }
+
+# dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)  # Database URL
+DATABASES["default"].update(db_from_env)
 
 
 # Password validation
